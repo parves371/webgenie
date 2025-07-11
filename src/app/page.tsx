29be6aeff908da70d3371dd3c81691de3f4f4b69
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-07-11 22:16:41 +0600

    feat: implement TRPC with React Query and add initial API routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const Page = async () => {
-  return <div>Helloow world</div>;
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "world" }));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense>Hellow</Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default Page;
